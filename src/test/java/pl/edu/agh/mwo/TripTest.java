224fd8764f427795848eb40b146ff3a2bd4035cb
Oliwia <olakatosz18@gmail.com>
2019-04-29 20:20:22 +0200

    Trip test

--- src/test/java/pl/edu/agh/mwo/TripTest.java
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -13,6 +13,17 @@ public class TripTest {
 	}
 
 	@Test
+    public void testTripEditing() {
+	    Trip trip = new Trip("Lorem", "Ipsum");
+        assertEquals("Lorem", trip.getName());
+        assertEquals("Ipsum", trip.getDescription());
+	    trip.setName("name");
+	    trip.setDescription("description");
+	    assertEquals("name", trip.getName());
+	    assertEquals("description", trip.getDescription());
+    }
+
+	@Test
     public void testAddPhoto() {
 	    Photo photo = new Photo();
 	    Trip trip = new Trip("nazwa", "opis");
